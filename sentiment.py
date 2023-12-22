9d88e9bdda102daac1194a12088e9c94bb697a7c
imkyznetsov <im.kyznetsov@gmail.com>
2023-12-22 21:07:09 +0300

    test

--- sentiment.py
+++
@@ -1,4 +0,0 @@
-from transformers import pipeline
-model = pipeline(model="seara/rubert-base-cased-russian-sentiment")
-model("Привет, ты мне нравишься!")
-# [{'label': 'positive', 'score': 0.9818321466445923}]
